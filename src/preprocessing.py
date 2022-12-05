98d6cba4a1b481b3a75e88ceb6ce68bfb57971e3
hoanganhminh01 <hoanganhminh01@gmail.com>
2022-12-04 21:43:52 -0800

    add preprocessing, model

---
+++ src/preprocessing.py
@@ -0,0 +1,24 @@
+import os
+from PIL import Image
+
+data_dir = '../data/'
+save_dir = '../data_preprocessed/'
+
+image_size = 800
+crop_size = 512
+
+if not os.path.isdir(save_dir):
+    os.mkdir(save_dir)
+
+img_list = os.listdir(data_dir)
+
+for i in range(10000):
+    img = Image.open(data_dir + img_list[i])
+    c_x = (img.size[0] - crop_size) // 2
+    c_y = (img.size[1] - crop_size) // 2
+    img = img.crop([c_x, c_y, c_x + crop_size, c_y + crop_size])
+    img = img.resize((image_size, image_size), Image.BILINEAR)
+    img.save(save_dir + img_list[i], 'JPEG')
+
+    if i % 1000 == 0:
+        print('Resizing %d images...' % i)
